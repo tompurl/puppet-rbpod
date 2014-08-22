8b91ce38831541f0fb2f1e9f942b52405e3006bd
Tom Purl <tom@tompurl.com>
2014-08-22 18:43:39 -0500

    Fixed a bug in the property name.

--- manifests/location.pp
+++ manifests/location.pp
@@ -2,6 +2,6 @@ class rbpod::location inherits rbpod {
   # This class handles all location-related settings.
 
     class { 'timezone':
-        timezone => $timeznone,
+        timezone => $timezone,
   }
 }
