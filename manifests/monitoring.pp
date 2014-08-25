10eb0b20c3187a6897883369bd9f9575bce7528d
Tom Purl <tom@tompurl.com>
2014-08-25 12:48:31 -0500

    Whoops, the monit class doesn't even need that parameter.

--- manifests/monitoring.pp
+++ manifests/monitoring.pp
@@ -11,7 +11,6 @@ class rbpod::monitoring inherits rbpod {
         smtp_port      => $smtp_port,
         smtp_user_name => $smtp_user_name,
         smtp_password  => $smtp_password,
-        email_hostname => $email_hostname,
         web_username   => $monit_web_user_name,
         web_password   => $monit_web_password,
     }
